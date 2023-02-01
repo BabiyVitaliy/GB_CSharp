03485dedc11912c88610418939dca366b3913ffd
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-01 23:54:25 +0300

    8 zadanie

---
+++ 008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Вывести на экран числа от -N до N
+int a;
+System.Console.Write("Введите число: ");
+a=Convert.ToInt32(Console.ReadLine());
+if (a<0) a = a * -1; 
+int b = a * -1;
+while (b<a)
+{
+    System.Console.WriteLine(b);
+    b++;
+}
