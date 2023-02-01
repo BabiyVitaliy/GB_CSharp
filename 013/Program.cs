93be4dc40f690eff9e2820a8d2930a4906a87d3a
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-02 00:30:44 +0300

    13 задача

---
+++ 013/Program.cs
@@ -0,0 +1,9 @@
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+int a=0;
+while (a<10)
+{
+    System.Console.Write("Введите число больше 10:");
+    a=Convert.ToInt32(Console.ReadLine());
+}
+if (a<100) System.Console.WriteLine($"{"Число: "}{a}{" без второй цифры: "}{a%10}");
+else System.Console.WriteLine($"{"Число: "}{a}{" без второй цифры: "}{a/100}{a%10}");
