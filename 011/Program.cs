a62c223c89b7805247c7c73e5a25fa2b3c96f8a4
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-02 00:06:54 +0300

    11 задачние

---
+++ 011/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+int a=0;
+
+while (a<10)
+{
+    System.Console.Write("Введите число больше 9:");
+    a=Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine($"{"Вторая цифра числа "}{a}{": "}{a/10%10}");
