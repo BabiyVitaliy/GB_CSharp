f6f705d5810bb08d06bdaad3722dc9a8e2cec475
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-02 00:02:33 +0300

    10 задача

---
+++ 010/Program.cs
@@ -0,0 +1,5 @@
+﻿// Вывести на экран последнюю цифру целого числа введенного с клавиатуры
+int a;
+System.Console.Write("Введите 1 число: ");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"{"Последняя цифра числа "}{a}{": "}{a%10}");
