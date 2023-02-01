83722019742ce80c7141180302cf10b4f64a3089
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-01 23:45:35 +0300

    6 zadanie

---
+++ 006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Написать программу вычисления значения функции y = sin(a). (Класс Math)
+double a;
+System.Console.Write("Введите число: ");
+a=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"{"Sin "}{a}{" = "}{Math.Sin(a)}");
