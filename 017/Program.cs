6b6178117b607af18bcbaa52ac23ad54eafa356a
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-03 22:58:41 +0300

    2 урок 2 задача

---
+++ 017/Program.cs
@@ -0,0 +1,8 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+double a,b;
+System.Console.Write("Введите 1 число: ");
+a=Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите 2 число: ");
+b=Convert.ToDouble(Console.ReadLine());
+if (Math.Sqrt(a)==b || Math.Sqrt(b)==a) System.Console.Write("Одно число является квадратом другого");
+else System.Console.Write("Числа не являются квадратом");
