54e622d1578faa5c2b0fe56af25f42cebbb3c8dd
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-03 22:51:09 +0300

    2 урок 1 задача

---
+++ 016/Program.cs
@@ -0,0 +1,5 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+    System.Console.Write("Введите число: ");
+    int a=Convert.ToInt32(Console.ReadLine());
+    if (a%7==0 && a%23==0) System.Console.Write("Число кратно и 7 и 23");
+    else System.Console.Write("Число не кратно 7 и 23");
