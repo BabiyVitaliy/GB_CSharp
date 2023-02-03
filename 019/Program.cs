395257439e3578fe51f2b6be63873d99663e8c47
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-03 23:31:14 +0300

    2 урок 4 задача

---
+++ 019/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа проверяет пятизначное число на палиндромом. Например: 12321
+int a;
+do
+{
+    System.Console.Write("Введите число: ");
+    a=Convert.ToInt32(Console.ReadLine());
+} while(a < 10000 || a > 99999);
+if (a/10000==a%10 && a/1000%10==a/10%10) System.Console.WriteLine($"{"Число: "}{a}{" является полиндромом"}");
+else System.Console.WriteLine($"{"Число: "}{a}{" не является полиндромом"}");
