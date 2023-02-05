37ce3e7eb1a9e133f8b6c89e49f4445175f319d9
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-05 22:08:24 +0300

    034 Задача

---
+++ 034/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить количество цифр в числе. Сделать подпрограмму.
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"{"В числе "}{a}{" - "}{HowMany(a)}{" цифер"}");
+
+
+
+
+int HowMany (int a)
+{
+    if (a < 10) 
+    {
+        return 1;
+    }
+    else
+    {
+        return 1 + HowMany(a/10);
+    }
+    
+}
