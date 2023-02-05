bfbaf4d6d28416f77e59937b2e854f2119e65b26
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-05 21:26:15 +0300

    3 урок_1 задача

---
+++ 030/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести на экран таблицу квадратов чисел от 1 до N
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<a; i++)
+{
+    System.Console.WriteLine($"{i}{"^3="}{Math.Pow(i,3)}");
+}
