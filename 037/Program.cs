08b8f7690839b1c38ae1cb5c4de8076b0b20e3d5
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-05 23:03:02 +0300

    036 Задача, вопрос по поводу заканчивающихся на четную цифру, что должгно заканчиваться число которое возводится в куб, или результат возведения числа в куб, я так понял что 1-ый варинт, т.к. в противном случае должно было быть окончание заканчивающиеся

---
+++ 037/Program.cs
@@ -0,0 +1,27 @@
+﻿//Глупая задача. Показать кубы чисел, заканчивающихся на четную цифру
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i =1; i<a+1; i++)
+{ 
+    if (Yes(i)) System.Console.WriteLine($"{"Куб числа "}{i}{" = "}{PowerUp(i)}");
+}
+
+
+double PowerUp(int a)
+{
+    return Math.Pow(a, 3);
+}
+
+
+bool Yes(int a)
+{
+    if (a % 2 == 0)
+        return true;
+    else
+        return false;
+}
+
+
+
