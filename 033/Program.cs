298776b5a8735b3e36bcd6dd162165d49934ef14
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-05 21:59:38 +0300

    033 Задача

---
+++ 033/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возведите число А в натуральную степень B используя цикл
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"{a}{" в степени "}{b}{" = "}{pow(a,b)}");
+int pow(int a, int b)
+{
+    int temp = 1;
+    for (int i = 0; i < b; i++)
+    {
+        temp *= a;
+    }
+    return temp;
+}
