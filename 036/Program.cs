be6490c63833345384471e15d7d8bb48346ee686
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-05 22:46:15 +0300

    036 Задача

---
+++ 036/Program.cs
@@ -0,0 +1,18 @@
+﻿// Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"{"Факториал числа "}{a}{" - "}{Fact(a)}");
+int Fact (int a)
+{
+    if (a == 1 ) 
+    {
+        return 1;
+    }
+    else
+    {
+        return a * Fact(a-1);
+    }
+    
+}
