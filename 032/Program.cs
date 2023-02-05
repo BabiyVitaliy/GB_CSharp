5bb376a1e819705e852a6f56012741135f390698
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-05 21:52:48 +0300

    032 задача

--- 032/Program.cs
+++ 032/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел от 1 до А
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = summ (a);
+System.Console.WriteLine($"{"Сумма чисел от 1 до "}{a}{" = "}{b}");
+
+int summ (int chislo)
+{
+    int now = 0;
+    while (chislo>0);
+    {
+        now += a;
+        a--;
+    }
+    
+    return now;
+}
