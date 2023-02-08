56231db373dd3f82a49c09217c6b18a95dedddb7
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-08 22:33:37 +0300

    Семинар 4 - 041 Задача

---
+++ 041/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+Random var = new Random();
+int[] nums = new int[8];
+for (int i = 0; i < 7; i++)
+{
+    if (var.Next(0, 10) < 6) nums[i] = 0;
+    else nums[i] = 1;
+    System.Console.Write($"{nums[i]} ");
+}
