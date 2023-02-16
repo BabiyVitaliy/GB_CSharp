36255ec7db232f0ed1d8cc3777b43deb3dc68813
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-16 23:31:23 +0300

    5 Семинар 061 Задача

--- 060/Program.cs
+++ 060/Program.cs
@@ -14,22 +14,22 @@ OutputArraw(userArrow);
 
 
 
-int[,] InitialRandomArraw (int m, int n,int min, int max)                      
+int[,] InitialRandomArraw(int m, int n, int min, int max)
 {
-    int[,] Arr=new int[m,n];
-    Random random=new Random();
-    for(int i=0;i<Arr.GetLength(0);i++)
-        for(int j=0;j<Arr.GetLength(1);j++)
-            Arr[i,j]=random.Next(min,max+1);
+    int[,] Arr = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < Arr.GetLength(0); i++)
+        for (int j = 0; j < Arr.GetLength(1); j++)
+            Arr[i, j] = random.Next(min, max + 1);
     return Arr;
 }
 
-void OutputArraw (int[,] a)
+void OutputArraw(int[,] a)
 {
-    for(int i=0;i<a.GetLength(0);i++)
+    for (int i = 0; i < a.GetLength(0); i++)
     {
-    for(int j=0;j<a.GetLength(1);j++)
-        System.Console.Write($"{a[i,j],6}");
-    System.Console.WriteLine();
+        for (int j = 0; j < a.GetLength(1); j++)
+            System.Console.Write($"{a[i, j],6}");
+        System.Console.WriteLine();
     }
 }
---
+++ 061/Program.cs
@@ -0,0 +1,32 @@
+﻿// 061 Показать двумерный массив размером m×n заполненный вещественными случайными числами
+
+System.Console.Write("Введите длину массива: ");
+int ArrawDlina = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите ширину массива: ");
+int ArrawShirina = Convert.ToInt32(Console.ReadLine());
+
+double[,] userArrow = InitialRandomDoubleArraw(ArrawDlina, ArrawShirina);
+OutputArraw(userArrow);
+
+
+
+double[,] InitialRandomDoubleArraw(int m, int n)
+{
+    double[,] Arr = new double[m, n];
+    Random random = new Random();
+    for (int i = 0; i < Arr.GetLength(0); i++)
+        for (int j = 0; j < Arr.GetLength(1); j++)
+            Arr[i, j] = random.NextDouble()*10;
+    return Arr;
+}
+
+void OutputArraw(double[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+            System.Console.Write($"{a[i, j],6:F2}");
+        System.Console.WriteLine();
+    }
+}
+
