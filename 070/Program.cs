cd7f6c32d01885e24ec424666b68c0c689eb8383
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-23 11:16:03 +0300

    071 зачада

--- 070/Program.cs
+++ 070/Program.cs
@@ -1,13 +1,7 @@
 ﻿// 70. Показать натуральные числа от 1 до N, N задано
 int N = 20, i=1;
-/* while (N < 2)
-{
-    System.Console.Write("Введите число больше 1: ");
-    N = Convert.ToInt32(Console.ReadLine());
-} */
+
 RecOutput(i,N);
-
-
 void RecOutput(int i, int N)
 {
     System.Console.Write($"{i,4}");
---
+++ 071/Program.cs
@@ -0,0 +1,9 @@
+﻿// 71. Показать натуральные числа от N до 1, N задано
+
+int N = 20, i=1;
+RecOutput(i,N);
+void RecOutput(int i, int N)
+{
+    System.Console.Write($"{N,4}");
+    if (i<N) RecOutput(i,--N);
+}
