3cf9017ebd56d5e62fbbeed42ea62eafebfddceb
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-23 11:20:02 +0300

    074 задача

---
+++ 074/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму цифр числа 
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write($"{"Сумма цифер числа "}{a}{" - "}{SummCiferChisla(a)}");
+int SummCiferChisla(int a)
+{
+    if (a < 10)
+    {
+        return a % 10;
+    }
+    else
+    {
+        return a % 10 + SummCiferChisla(a / 10);
+    }
+
+}
