f3c375ccc62c59ff7607abe064f5199fad56d67f
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-23 22:05:36 +0300

    084 задача

---
+++ 084/Program.cs
@@ -0,0 +1,13 @@
+﻿// 84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. Вычислить сумму цифр введенного числа
+System.Console.Write("Введите текст с клавиатуры: ");
+string num = Console.ReadLine();
+int temp = 0;
+if (!Int32.TryParse(num, out temp))
+    Console.WriteLine("Это не является верной записью целого");
+else
+{
+    int sum = 0;
+    foreach (char digit in num)
+        sum += digit - 48;
+    Console.WriteLine(sum);
+}
