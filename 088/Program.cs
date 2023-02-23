8793e67202f24e4e2cc1836e644f2644459c5927
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-23 23:24:17 +0300

    088 Задача

---
+++ 088/Program.cs
@@ -0,0 +1,12 @@
+﻿// 88. Заменить все вхождения подстроки w в строке st на строку v. 
+// Вывести на экран первоначальную строку и конечную строку
+
+System.Console.Write("Введи строку: ");
+string stroka = Console.ReadLine();
+System.Console.Write("Что меняем: ");
+string textChange = Console.ReadLine();
+System.Console.Write("На что меняем: ");
+string textSwap = Console.ReadLine();
+System.Console.WriteLine($"Оригинальный текст: {stroka}");
+System.Console.WriteLine($"Заменённый текст: {stroka.Replace(textChange, textSwap)}");
+
