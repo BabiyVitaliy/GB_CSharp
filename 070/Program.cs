243b0fd76f740e059b848afe3300cb4be6015f63
Babiy Vitaliy <AionVitaliyKam@gmail.comm>
2023-02-23 11:06:22 +0300

    7 семинар, 070 задача

---
+++ 070/Program.cs
@@ -0,0 +1,15 @@
+﻿// 70. Показать натуральные числа от 1 до N, N задано
+int N = 0, i=1;
+while (N < 2)
+{
+    System.Console.Write("Введите число больше 1: ");
+    N = Convert.ToInt32(Console.ReadLine());
+}
+RecOutput(i,N);
+
+
+void RecOutput(int i, int N)
+{
+    System.Console.Write($"{i,4}");
+    if (i<N) RecOutput(i+1,N);
+}
